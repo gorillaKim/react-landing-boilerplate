077e3b4fa2e8adac1c18ccfbbe7047cb2f03cd1b
gorillaKim <yhkim@madup.com>
2021-05-11 16:26:57 +0900

    feat: installed styled-components @types/styled-components

---
+++ pages/index02.tsx
@@ -0,0 +1,15 @@
+function Index (props) {
+  const { message } = props;
+  return <div>
+    Welcome to {message}
+  </div>;
+};
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      message: `Next.js!`
+    }
+  };
+};
+export default Index;
