a31e20489c9812609aac0d25d48fba98a887599a
gorillaKim <yhkim@madup.com>
2021-05-24 15:18:35 +0900

    feat: react 모듈 불러오기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import styled from "styled-components"
 import GlobalStyle from "../src/styles/GlobalStyle";
 import {Container} from "../src/components/Styled/Layout";
---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = "https://office.lever.me"
+const SecureToken = "7a783db8-8a8f-4857-b044-83cf99a4e07c"
+
+const Contact = {
+  postContact: ''
+}
