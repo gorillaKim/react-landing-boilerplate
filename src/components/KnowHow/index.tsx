93f1c14373e26ea890b322406aaa2a770397fe42
admin <scnam@madup.com>
2021-05-24 16:17:03 +0900

    feat: react-slider 추가

---
+++ src/components/KnowHow/index.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement, useState } from 'react'
+import styled from "styled-components"
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function SimpleSlider() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <h3 style={{backgroundColor: 'green'}}>1</h3>
+      </div>
+      <div >
+        <h3 style={{backgroundColor: 'orange'}}>2</h3>
+      </div>
+      <div >
+        <h3 style={{backgroundColor: 'green'}}>3</h3>
+      </div>
+      <div >
+        <h3 style={{backgroundColor: 'orange'}}>4</h3>
+      </div>
+      <div >
+        <h3 style={{backgroundColor: 'green'}}>5</h3>
+      </div>
+      <div >
+        <h3 style={{backgroundColor: 'orange'}}>6</h3>
+      </div>
+    </Slider>
+  );
+}
+const KnowHowSection = styled.div`
+  & .slick-prev:before, 
+  & .slick-next:before {
+    color: var(--cornflower)
+  }
+`
+const KnowHow = () => {
+  return (
+    <KnowHowSection>
+
+      <div style={{width: '80%', margin: '0 auto'}}>
+        <SimpleSlider />
+      </div>
+    </KnowHowSection>
+  )
+}
+
+export default KnowHow
