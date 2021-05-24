ddf050d559be49524f8d6c10231e4bd02ae6a62a
admin <scnam@madup.com>
2021-05-24 22:35:09 +0900

    feat: 노하우 슬라이더 완성

---
+++ src/types/knowhow.ts
@@ -0,0 +1,12 @@
+export interface KnowHowProps {
+  demo: {
+    id: string
+    url: string
+    href: string
+    content: string
+  }[]
+}
+export interface KnowHowSlideProps {
+  demo: any
+  settings: any
+}
