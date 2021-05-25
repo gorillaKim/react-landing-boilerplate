b1e7235e652e1dcc3d68fd506e68ead036c50389
gorillaKim <yhkim@madup.com>
2021-05-25 10:29:15 +0900

    feat: 불필요한 코드 제거

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
+const path = require('path')
 module.exports = {
   // "distDir": "build",
   // "exportTrailingSlash": true,
   // "assetPrefix": "https://~~~",
+
   basePath: '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
