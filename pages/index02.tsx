e784e15f5f45f7dc4b362a05fbe4a39c8c151fb4
gorillaKim <yhkim@madup.com>
2021-05-26 16:13:57 +0900

    fixed: 잘못된 데이터 수정

--- pages/index02.tsx
+++ pages/index02.tsx
@@ -1,48 +1,42 @@
-import React from "react"
-import styled from "styled-components"
-import GlobalStyle from "../src/styles/GlobalStyle"
-import {Container} from "../src/components/Styled/Layout"
+import React from 'react'
+import styled from 'styled-components'
+import GlobalStyle from '../src/styles/GlobalStyle'
 import type { AppContext } from 'next/app'
-import MainB from "../src/components/MainArticle/MainB"
-import BannerContainer from "../src/containers/banner/BannerContainer"
-import Navbar from "../src/components/Navbar"
-import Introduce from "../src/components/Introduce";
-import FaqContainer from "../src/containers/Faq/FaqContainer";
-import Footer from "../src/components/Footer";
+import MainB from '../src/components/MainArticle/MainB'
+import BannerContainer from '../src/containers/banner/BannerContainer'
+import Navbar from '../src/components/Navbar'
+import Introduce from '../src/components/Introduce'
+import FaqContainer from '../src/containers/Faq/FaqContainer'
+import Footer from '../src/components/Footer'
+import SuccessStoryContainer from '../src/containers/successStory/SuccessStoryContainer'
+import KnowHowContainer from '../src/containers/knowHow/KnowHowContainer'
+import MissionContainer from '../src/containers/mission/MissionContainer'
 
 const PageContainer = styled.div`
   position: relative;
 `
-interface IIndex {
-}
+interface IIndex {}
 
-const Index = (props:IIndex) => {
+const Index = (props: IIndex) => {
   return (
     <PageContainer>
       <GlobalStyle />
-      <BannerContainer/>
-      <Navbar/>
+      <BannerContainer />
+      <Navbar />
       <MainB />
-      <Container style={{backgroundColor: "orange", height: "fit-content", minHeight: '444px'}}>
-        성공스토리
-      </Container>
-      <Introduce/>
-      <Container style={{backgroundColor: "orange", height: "fit-content", minHeight: '444px'}}>
-        노하우
-      </Container>
-      <FaqContainer/>
-      <Container style={{backgroundColor: "orange", height: "fit-content", minHeight: '444px'}}>
-        미션
-      </Container>
-      <Footer/>
+      <SuccessStoryContainer />
+      <Introduce />
+      <KnowHowContainer />
+      <FaqContainer />
+      <MissionContainer />
+      <Footer />
     </PageContainer>
-  );
-};
+  )
+}
 
-export const getStaticProps = async (context:AppContext) => {
+export const getStaticProps = async (context: AppContext) => {
   return {
-    props: {
-    }
+    props: {},
   }
 }
-export default Index;
+export default Index
